7c72d218d939803695d144d7616d36a20e10aefb
kjurczyn <01169180@pw.edu.pl>
2024-05-07 21:14:49 +0200

    added GameState

--- Haskell/src/Game.hs
+++ Haskell/src/Game.hs
@@ -1,2 +1,26 @@
 module Game where
 
+import qualified Items
+import qualified Locations
+
+import Data.Maybe (isNothing, fromMaybe, fromJust)
+
+data Event = RatKingDefeated deriving Eq
+
+data GameState = GameState{
+    inventory::[Items.Item],
+    currentLocation::Locations.Location,
+    message::String,
+    locations::[Locations.Location],
+    events::[Event]
+}
+
+-- describe::GameState->String->GameState
+-- describe p s = fromMaybe "You are not holding this item!" (Items.getItemDescription (inventory p) s) 
+
+-- -- TODO: Function to pick up items
+-- pickUp::GameState->String->GameState
+-- pickUp p s = 
+--     -- let h = Items.getItem (Locations.items (currentLocation p)) s in
+--     --     if isNothing h then "This is item is not here!"
+--     --     
